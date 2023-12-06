45ca49d626f369ded9b7553d8c738aef50fdc67b
Chris McKinnon <cmckin18@vols.utk.edu>
2023-12-05 19:40:29 -0500

    fix

--- detection/detection.py
+++ detection/detection.py
@@ -8,15 +8,21 @@ while cap.isOpened():
     (_,frame) = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face=0
+    face_detected=0
     area=0
     i=0
     faces = face_detect.detectMultiScale(gray,1.1, 4 )
     for (x,y, w, h) in faces:
         if h*w > area:
+            face_detected=1
             area = h*w
-            face=i
+            face=(x,y,w,h)
+        i+=1
+   # print(frame.shape)
 
-        cv2.rectangle(frame, pt1 = (x,y),pt2 = (x+w, y+h), color = (255,0,0),thickness =  3)
-    cv2.imshow("window", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if face_detected != 0:
+        x,y,w,h = face
+        middle_face = (x+x+w)/2
+        if abs(middle_face-(640/2))<20:
+            print("shoot")
+    
